72f98fa2dc568002d6854f502bc4e892ef0dee0f
srhpsnui <sarahpisini@wireless-nat-inside.uiowa.edu>
2017-10-28 10:22:56 -0500

    Initial Commit

---
+++ Global Anti-Allergy/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Global Anti-Allergy
+//
+//  Created by Sarah Pisini on 10/27/17.
+//  Copyright © 2017 Sarah Pisini. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
